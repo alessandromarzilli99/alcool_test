69da39cdf08b99b143541ec112dbde0e21f9e3ef
alessandromarzilli99 <alessandromarzilli99@gmail.com>
2022-05-05 19:42:22 +0200

    add functions for statistics

--- WebApp/index.js
+++ WebApp/index.js
@@ -4,6 +4,9 @@ var yValues1 = [0, 0, 0, 0, 0, 0, 0];
 var yValues2 = [0, 0, 0, 0, 0, 0, 0];
 var ch1;
 var ch2;
+time_slot = [0,0,0,0,0] //[8-12, 12-17, 17-20, 20-00, 00-8]
+let pos_tests = 0.0
+let neg_tests = 0.0
     
 function publish_topic_to_iotcore(msg){
     var myHeaders = new Headers();
@@ -20,7 +23,6 @@ function publish_topic_to_iotcore(msg){
     .then()
 }
 
-
 function compare( a, b ) {
     if ( a.ts < b.ts ){
       return -1;
@@ -29,7 +31,6 @@ function compare( a, b ) {
       return 1;
     }
     return 0;
-  
 }
 
 function update_chart_values(d,val){
@@ -50,9 +51,11 @@ function update_chart_values(d,val){
     }
     if(val<= 450){
         ch1.data.datasets[0].data[x] = ch1.data.datasets[0].data[x]+1
+        neg_tests += 1
     }
     else{
         ch2.data.datasets[0].data[x] = ch2.data.datasets[0].data[x]+1
+        pos_tests += 1
     } 
     ch1.update();
     ch2.update();
@@ -110,13 +113,51 @@ function update_chart_labels(date){
         
         ch1.update();
         ch2.update();
+    }    
+}
+
+function get_timeslot(hour){
+    if(hour>=8 && hour<12){
+        time_slot[0]=time_slot[0]+1
+    }
+    else if(hour>=12 && hour<17){
+        time_slot[1]=time_slot[1]+1
+    }
+    else if(hour>=17 && hour<20){
+        time_slot[2]=time_slot[2]+1
+    }
+    else if(hour>=20 && hour<24){
+        time_slot[3]=time_slot[3]+1
+    }
+    else if(hour>=0 && hour<8){
+        time_slot[4]=time_slot[4]+1
     }
 
+    max = Math.max(...time_slot)
+    index = time_slot.indexOf(max)
+    switch (index) {
+        case 0:
+            slot = "08-12";
+            break;
+        case 1:
+            slot = "12-17";
+            break;
+        case 2:
+            slot = "17-20";
+            break;
+        case 3:
+            slot = "20-24";
+            break;
+        case 4:
+            slot = "00-8";
+            break;
+    }
+    return slot
 }
 
 function displayData(data){
     table = document.getElementById("info")
-    
+
     values = []
     data = JSON.parse(data);
     
@@ -136,6 +177,8 @@ function displayData(data){
         date_test = new Date(parseInt(values[i]["ts"]))
         day_week = date_test.getDay()
         day_month = date_test.getDate()
+        hour = date_test.getHours()
+        minutes = date_test.getMinutes()
         
         if(day_month != current_date.getDate() && day_week==current_date.getDay()){
             continue;
@@ -145,10 +188,10 @@ function displayData(data){
 
             if(day_week==current_date.getDay()){
                 if(date_test.getMinutes()<10){
-                    time=date_test.getHours().toString() + ":0" + date_test.getMinutes().toString()
+                    time=hour.toString() + ":0" + minutes.toString()
                 }
                 else{
-                    time =date_test.getHours().toString() + ":" + date_test.getMinutes().toString()
+                    time =hour.toString() + ":" + minutes.toString()
                 }
                 
                 row = table.insertRow(1);
@@ -158,11 +201,17 @@ function displayData(data){
                 cell2.innerHTML = values[i]["alcool_level"];
             }
             
+            slot = get_timeslot(hour)
 
         }
    
     }
-    
+
+    document.getElementById("timeslot").innerHTML = slot
+    document.getElementById("neg_tests").innerHTML = neg_tests
+    document.getElementById("pos_tests").innerHTML = pos_tests
+    percentage = (pos_tests/(neg_tests+pos_tests))*100
+    document.getElementById("percentage").innerHTML = percentage.toFixed(2) + "%"
     
 }
 
